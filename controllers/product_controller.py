24f4f15230e413b3470db042e216cab82c7ad943
Vansh <vansh.malhotra@seaflux.tech>
2024-03-28 18:32:29 +0530

    feat: add search product feature

--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from config.database import engine
 from sqlalchemy import select
 from helpers.api_helper import APIHelper
@@ -18,3 +19,16 @@ def create_product(request:ProductModel, token: str):
                 name=request.name, category=request.category,description=request.description,price=request.price,rating=request.rating,discount=request.discount))
         db.commit()        
     return APIHelper.send_success_response(successMessageKey="Product Created Successfully")
+
+def product_search(name: Optional[str] = None, category: Optional[str] = None):
+    with engine.connect() as db:
+        query = select(product_table)
+        if name or category:
+            query = query.where(
+            (product_table.c.name.like(f"%{name}%")) if name else True,
+            (product_table.c.category.like(f"%{category}%")) if category else True
+            )
+        products = db.execute(query).fetchall()
+        if not products:
+            return APIHelper.send_error_response(errorMessageKey="Product or Category Not Found")
+        return APIHelper.send_success_response(data=[ProductModel(**row._mapping) for row in products], successMessageKey="Product Found")
--- routes/product.py
+++ routes/product.py
@@ -1,8 +1,9 @@
 # routes/product.py
-from fastapi import APIRouter, Depends
-from controllers.product_controller import create_product
-from helpers.token_helper import get_token,get_current_user
-from dtos.product_models import ProductModel
+from typing import Optional
+from fastapi import APIRouter, Depends, Query
+from controllers.product_controller import create_product,product_search
+from helpers.token_helper import get_current_user
+from dtos.product_models import ProductModel,ProductSearch
 router = APIRouter(
    
     tags=['Product']
@@ -11,3 +12,6 @@ router = APIRouter(
 @router.post('/create_product')
 def create_product_route(request: ProductModel,token: str = Depends(get_current_user)):
     return create_product(request,token)
+@router.get('/search_product')
+def search_product_route(name: Optional[str] = Query(None), category: Optional[str] = Query(None)):
+    return product_search(name, category)
